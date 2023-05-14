0bed021a8e6e02f8fd94e6bdf9abf9a1d5364823
Andrew Rubenstein <andjruben@gmail.com>
2023-05-14 17:59:13 -0400

    feat: do not use mnemonic
    improves performance by 20x +. calls to mnemonicToSeed would take 9-13ms on average, which is much too long for this program.
    
    removing this takes the execution time to sub 1ms on average. :raised_hands:
    
    also removes dependency libsodium-wrappers-sumo in favor of the libsodium-wrappers package.

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 import chalk from 'chalk';
-import _sodium from 'libsodium-wrappers-sumo';
-import { generateMnemonic, mnemonicToSeed } from 'bip39';
+import _sodium from 'libsodium-wrappers';
 import { b58cencode, prefix } from '@taquito/utils';
 import { Logger } from './logger.js';
 
-const generateKeys = async ({ mnemonic, sodium }) => {
-  const seed = (await mnemonicToSeed(mnemonic)).slice(0, 32);
-  const kp = sodium.crypto_sign_seed_keypair(seed);
+const generateKeys = ({ sodium }) => {
+  const kp = sodium.crypto_sign_keypair();
 
   const secretKey = b58cencode(kp.privateKey, prefix.edsk);
   const publicKey = b58cencode(kp.publicKey, prefix.edpk);
   const publicKeyHash = b58cencode(sodium.crypto_generichash(20, kp.publicKey), prefix.tz1);
 
   return {
-    mnemonic,
     secretKey,
     publicKey,
     publicKeyHash,
   }
+}
+
+const findHashes = async ({ logger, searchTerm }) => {
+  let ticks = 0;
+
+  await _sodium.ready;
+  const sodium = _sodium;
+
+  const tick = async () => {
+    ticks++;
+
+    logger.log({ 
+      header: 'Progress: ', 
+      message: `${chalk.green(ticks + ' keys checked')}`
+    });
+    
+    //only supports tz1 prefix for now
+    const toFind = 'tz1' + searchTerm;
+
+    const keys = generateKeys({ sodium });  
+    const isMatch = toFind === keys.publicKeyHash.substring(0, toFind.length);
+
+    if(!isMatch) {
+      // no match found, next tick
+      setImmediate(tick);    
+    } else {
+      const secretKey = `${chalk.green('Secret key:')} ${keys.secretKey}`;
+      const publicKey = `${chalk.green('Public key:')} ${keys.publicKey}`;
+      const publicKeyHash = `${chalk.green('Address:')} ${keys.publicKeyHash}`;
+
+      logger.log({
+        header: `Match found after checking ${ticks} hashes.\n`, 
+        message: `${publicKeyHash}\n${secretKey}\n${publicKey}\n`,
+      })
+    }
+  }
+  
+  tick();
 }
 
 const main = async () => {
@@ -34,45 +69,6 @@ const main = async () => {
 
   const searchTerm = process.argv[2];
   await findHashes({ logger, searchTerm });
-}
-
-const findHashes = async ({ logger, searchTerm }) => {
-  let ticks = 0;
-
-  await _sodium.ready;
-  const sodium = _sodium;
-
-  const tick = async () => {
-    ticks++;
-
-    const mnemonic = generateMnemonic();
-    const keys = await generateKeys({ mnemonic, sodium });  
-
-    //only supports tz1 prefix for now
-    const toFind = 'tz1' + searchTerm;
-
-    logger.log({ 
-      header: 'Progress: ', 
-      message: `${chalk.green(ticks + ' keys checked')}`
-    });
-  
-    if(keys.publicKeyHash.indexOf(toFind) === -1) {
-      // no match found, next tick
-      setImmediate(tick);
-    } else {
-      const mnemonic = `${chalk.green('Mnemonic:')} ${keys.mnemonic}`;
-      const secretKey = `${chalk.green('Secret key:')} ${keys.secretKey}`;
-      const publicKey = `${chalk.green('Public key:')} ${keys.publicKey}`;
-      const publicKeyHash = `${chalk.green('Address:')} ${keys.publicKeyHash}`;
-
-      logger.log({
-        header: `Match found after checking ${ticks} hashes.\n`, 
-        message: `${publicKeyHash}\n${mnemonic}\n${secretKey}\n${publicKey}\n`,
-      })
-    }
-  }
-  
-  tick();
 }
 
 main();
